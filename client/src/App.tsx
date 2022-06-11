f1f49af8be21a2a9bb789fac84ec375cbc7a598d
Christopher Venczel <christopher.venczel@outlook.com>
2022-06-10 20:48:08 -0400

    POST working (at least locally)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,34 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+interface TreonData {
+  timestamp: number;
+}
+
 const App = () => {
 
-  const [data, setData] = useState("temporary");
+  const [data, setData] = useState<TreonData[]>([]);
 
   useEffect(() => {
     axios.get("/treonData").then((res: any) => {
-      const data = res.data;
+      const data = res.data.allData;
       setData(data);
     });
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Yeet <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div>{data}</div>
-      </header>
+      <h1>
+          Treon Data
+        </h1>
+      <div>
+          {data.map((d: TreonData, i: number) => {
+            return (
+              <div key={i}>{d.timestamp}</div>
+            );
+          })}
+        </div>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -17,15 +18,61 @@ app.get('/', (req, res) => {
   res.sendFile(process.cwd() + "/client/build/index.html");
 });
 
+
+// Get and set treonData
 app.get('/treonData', (req, res) => {
-  res.send("Yo what up!");
+  // Send the current data to the front end
+  const currentData = JSON.parse(fs.readFileSync(__dirname + '/data.json', 'utf8'));
+  res.send(currentData);
 });
 
+const formatAMPM = (date) => {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes.toString().padStart(2, '0');
+  seconds = seconds.toString().padStart(2, '0');
+  let strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+  return strTime;
+}
+
 app.post('/treonData', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
+  const newData = req.body;
+  const currentData = JSON.parse(fs.readFileSync(__dirname + '/data.json', 'utf8'));
+
+  // Get the date as an easily readable string
+  // Example: January 2, 2022 - 10:15:32 PM
+  const now = new Date();
+  const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  const month = monthNames[now.getMonth()];
+  const day = now.getDate();
+  const year = now.getFullYear();
+  const time = formatAMPM(now);
+  const timestamp = `${month} ${day}, ${year} - ${time}`;
+
+  const allData = JSON.stringify({
+    allData: [
+      ...currentData.allData,
+      {
+        timestamp: timestamp,
+        data: newData
+      }
+    ]
+  });
+
+  try {
+    fs.writeFileSync(__dirname + '/data.json', allData);
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.sendStatus(200);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
